25b4fda365efc6c961dc9b4652f8963004edcc39
eun2i <yesey109@naver.com>
2023-06-04 19:44:22 +0900

    modify

--- Python 기초설정.md
+++ Python 기초설정.md
@@ -1,5 +1,4 @@
-### Pycharm 에서 프로젝트별로 아나콘다 가상환경 사용
-
+## 1.  Pycharm 에서 프로젝트별로 아나콘다 가상환경 사용
 ### 가상환경 생성 방법
 1. Anaconda 설치
 2. Anaconda Prompt 에서 가상환경 생성
@@ -15,3 +14,10 @@ conda env list
 conda activate main
 conda install pandas
 ```
+
+## 2. 패키지, 모듈, 함수 차이 
+
+- 패키지(Package) : 패키지는 관련된 모듈들을 그룹화하여 함께 제공
+- 모듈(Module) : 모듈은 패키지의 구성 요소로, 단일 파이썬 파일에 코드가 저장된 형태. 모듈은 특정 기능 또는 작업을 수행하기 위한 함수, 클래스, 변수 등을 포함
+- 함수(Function) : 함수는 재사용 가능한 코드 블록으로, 특정 작업을 수행하기 위해 설계
+- scipy는 패키지, stats는 모듈, ttest는 함수
--- 빅분기 실기.py
+++ 빅분기 실기.py
@@ -1,21 +1,31 @@
 
 ### 빅데이터분석기사 실기 공부(23년 개정 ver)
-import pandas as pd
-
-# 1과목
-import numpy as np
-import pandas as pd
-help(np)
-dir(np)
-
-pip install --upgrade pandas
 
 import pandas as pd
 import numpy as np
-print("pandas version:", pd.__version__)
-print("numpy version:", np.__version__)
 
-pip install --upgrade pip
-# 2과목
+### 도움말 사용 방법
 
-# 3과목
+# 검색을 할 수 없기 때문에, 파이썬 내부의 도움말 함수를 이용하여야한다 -> help, dir
+# help는 부연설명이 더 많고, dir은 그 안의 subpackage, module 리스트를 보여준다
+
+import scipy
+dir(scipy)
+help(scipy)
+
+# stats가 필요한 모듈인 경우, scipy에서 stats 불러오기
+
+from scipy import stats
+dir(stats)
+help(stats)
+
+# help의 example을 통해 함수에 필요한 변수, 사용법을 확인
+help(stats.ttest_ind)
+
+### 1과목
+
+### 2과목
+
+### 3과목
+
+
