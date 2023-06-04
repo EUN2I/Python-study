307656c2f7c8c078dfc9fad7b08bf2c3a82d2733
eun2i <yesey109@naver.com>
2023-06-04 17:25:08 +0900

    modify

---
+++ 빅분기 실기.py
@@ -0,0 +1,26 @@
+
+### 빅데이터분석기사 실기 공부(23년 개정 ver)
+import pandas as pd
+2+3
+
+
+pip install pandas
+pip install numpy
+
+# 1과목
+import numpy as np
+import pandas as pd
+help(np)
+dir(np)
+
+pip install --upgrade pandas
+
+import pandas as pd
+import numpy as np
+print("pandas version:", pd.__version__)
+print("numpy version:", np.__version__)
+
+pip install --upgrade pip
+# 2과목
+
+# 3과목
